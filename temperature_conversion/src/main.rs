5a16fcb207efdc3fee15bc8da0a014173a3b94fd
João Filipe Storarri <13003392+flpStrri@users.noreply.github.com>
2023-02-26 18:59:58 -0300

    Add tests to temperature conversion

--- temperature_conversion/src/main.rs
+++ temperature_conversion/src/main.rs
@@ -1,5 +1,9 @@
 use std::io;
 
+fn fahrenheit_to_celcius(fahrenheit_temperature: f64) -> f64 {
+    (fahrenheit_temperature - 32.0) * (5.0/9.0)
+}
+
 fn main() {
     println!("What is the temperature in Fahrenheit?");
 
@@ -11,6 +15,16 @@ fn main() {
 
     let temperature: f64 = temperature.trim().parse().expect("Failed to read number");
 
-    let celcius = (temperature - 32.0) * (5.0/9.0);
+    let celcius = fahrenheit_to_celcius(temperature);
     println!("So... its {celcius}C in the right metric system");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_adds_two() {
+        assert_eq!(fahrenheit_to_celcius(32.0), 0.0);
+    }
 }
